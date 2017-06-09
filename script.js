4e77b1b0a60fb6f24e68592ef70aa819f269f70c
vignesh109 <vignesh109@gmail.com>
2017-06-09 13:26:18 +0530

    Create script.js

--- script.js
+++ script.js
@@ -4,74 +4,61 @@ $(function() {
 
 
 
-  $(".changeKeyType").on('click', function(e) {
-    $("#hiddenInput").val('');
-    var button = $(this);
-    idPrefix = (this.id).split('-')[0];
-    var inputsuffix = button.text().trim();
-    var toggleText = (inputsuffix == '123' ? 'abc' : '123');
-    var toggleType = (inputsuffix == '123' ? 'number' : 'text');
-    $("#hiddenInput").attr('type', toggleType);
-    originalVal = $("#" + idPrefix).val();
-    $("#hiddenInput").val(originalVal ); 
-    $(this).text(toggleText);
-    oldLength = 0;
-    if ($("#hiddenInput").val() !== undefined) {
-      oldLength = $("#hiddenInput").val().length;
-    }
-    $("#" + idPrefix).addClass("activeInput");
-    $("#" + idPrefix).on('focus', function() {
-      button.text('abc');
-      $("#hiddenInput").attr('type', 'number');
-    });
-    maxLength = $("#" + idPrefix).attr("maxlength");
-    $("#hiddenInput").focus();
-  });
-  $(".changeKeyInput").on('click focus', function() {
-    $("#hiddenInput").focus().val('');
-    $("#hiddenInput").attr('type', 'number');
-    idPrefix = (this.id).split('-')[0];
-    originalVal = $(this).val();
-    $("#hiddenInput").val(originalVal ); 
-    oldLength = 0;
-    if ($("#hiddenInput").val() !== undefined) {
-      oldLength = $("#hiddenInput").val().length;
-    }
-    maxLength = $(this).attr("maxlength");
-  });
+ $(".changeKeyType").on('click', function(e) {
+	    $("#hiddenInput").val(0);
+	    var button = $(this);
+	    idPrefix = (this.id).split('-')[0];
+	    var inputsuffix = button.text().trim();
+	    var toggleText = (inputsuffix == '123' ? 'abc' : '123');
+	    var toggleType = (inputsuffix == '123' ? 'number' : 'text');
+	    $("#hiddenInput").attr('type', toggleType);
+	    originalVal = $("#" + idPrefix).val();
+	    $(this).text(toggleText);
+	    oldLength = 0;
+	    if ($("#hiddenInput").val() !== undefined) {
+	        oldLength = $("#hiddenInput").val().length;
+	    }
+	    $("#" + idPrefix).addClass("activeInput");
+	    $("#" + idPrefix).on('focus', function() {
+	        button.text('abc');
+	        $("#hiddenInput").attr('type', 'number');
+	    });
+	
+	    maxLength = $("#" + idPrefix).attr("maxlength");
+	    $("#hiddenInput").focus();
+	});
+	
+	$(".changeKeyInput").on('click focus', function() {
+	    $("#hiddenInput").focus().val(0);
+	    $("#hiddenInput").attr('type', 'number');
+	    idPrefix = (this.id).split('-')[0];
+	    originalVal = $(this).val();
+	    oldLength = 0;
+	    if ($("#hiddenInput").val() !== undefined) {
+	        oldLength = $("#hiddenInput").val().length;
+	    }
+	    maxLength = $(this).attr("maxlength");
+	});
 
   $("#hiddenInput").off().on('keyup', function(event) {
-    console.log("keyCode: " + event.which);
-    // console.log(event);
-
-    var charCode = (typeof event.which == "number") ? event.charCode : event.keyCode;
-    // Allow non-printable keys
-    if (!charCode || charCode == 8 /* Backspace */ ) {
-      alert('backspace :' + charCode);
-    }
-    var typedChar = String.fromCharCode(charCode);
-    // Allow numeric characters
-    if (/\d/.test(typedChar)) {
-      alert('typedChar' + typedChar);
-    }
-
-    var actualLength = $("#" + idPrefix).val().length;
-    var newVal = $(this).val();
-    var newLength = newVal.length;
-    if (actualLength < maxLength && oldLength < newLength) {
-      originalVal += newVal[newVal.length - 1];
-      $("#" + idPrefix).val(originalVal);
-      oldLength = newVal.length;
-    } else if (charCode == 8) {
-      //
-      alert('found backspace' + " And pre Val :" + originalVal);
-      originalVal = originalVal.slice(0, originalVal.length - 1);
-      $("#" + idPrefix).val(originalVal);
-      // alert('after deleting :'+originalVal);
-      oldLength = newVal.length;
-    }
-    //alert('current val: '+$("#" + idPrefix).val());
-  });
+//		alert('keyup triggered');
+		var actualLength = $("#" + idPrefix).val().length;
+		var newVal = $(this).val();
+		var newLength = newVal.length;
+		if (actualLength < maxLength && newLength == 2) {
+			originalVal += newVal[newLength - 1];
+			$("#" + idPrefix).val(originalVal);
+			newVal = newVal.slice(0, -1);
+			$(this).val(newVal);
+		} else if(actualLength == maxLength && newLength >= 2){
+			$(this).val(0);
+		} else if(newLength == 0){
+//			alert('backspace');
+			originalVal = originalVal.slice(0, -1);
+			$("#" + idPrefix).val(originalVal)
+			$(this).val(0);
+		}
+	});
 
 
 
